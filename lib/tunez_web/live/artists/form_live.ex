6799fbbe659aa287eafdda3ba746941c35a508ca
Amit-Giri07 <amitgiri20620316@gmail.com>
2025-11-27 15:56:30 +0545

    Integrating a Form into a liveview

--- lib/tunez_web/live/artists/form_live.ex
+++ lib/tunez_web/live/artists/form_live.ex
@@ -2,7 +2,7 @@ defmodule TunezWeb.Artists.FormLive do
   use TunezWeb, :live_view
 
   def mount(_params, _session, socket) do
-    form = %{}
+    form = Tunez.Music.form_to_create_artist()
 
     socket =
       socket
@@ -37,7 +37,12 @@ defmodule TunezWeb.Artists.FormLive do
     """
   end
 
-  def handle_event("validate", %{"form" => _form_data}, socket) do
+  def handle_event("validate", %{"form" => form_data}, socket) do
+    socket =
+      update(socket, :form, fn form ->
+        AshPhoenix.Form.validate(form, form_data)
+      end)
+
     {:noreply, socket}
   end
 
